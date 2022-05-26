ff848b6e28ad849c78511491329a5b6b18a73943
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-05-27 01:53:45 +0900

    [Add]メール機能実装

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -2,8 +2,8 @@ class TaskMailer < ApplicationMailer
   def creation_email(task)
     @task = task
     mail(
-      subject: "タスク作成完了メール"
-      to: "user@example.com"
+      subject: "タスク作成完了メール",
+      to: "1@gmail.com",
       from: "taskleaf@example.com"
     )
   end
